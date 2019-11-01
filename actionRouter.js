c85a3e0cac3f22cc12f187103a548eb3fd62413a
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-01 10:20:54 +0000

    created a get endpoint to fetch actions with specified ids

--- actionRouter.js
+++ actionRouter.js
@@ -16,5 +16,25 @@ actionRouter.get("/", (req, res) => {
       });
   });
 
+  actionRouter.get("/:id", (req, res) => {
+    Actions.get(req.params.id)
+      .then(action => {
+        if (!action) {
+          res.status(400).json({
+            message: "The action with the specified ID does not exist."
+          });
+        } else {
+          res.status(200).json({
+            action
+          });
+        }
+      })
+      .catch(() => {
+        res.status(500).json({
+          error: "this content could not be retrieved"
+        });
+      });
+  });
+
 
 module.exports = actionRouter;

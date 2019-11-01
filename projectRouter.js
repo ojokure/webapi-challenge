c24221855a7d040a2c33d7afe6a84b4cd3e7c8bc
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-01 09:27:57 +0000

    created apost endpoint to post projects

--- projectRouter.js
+++ projectRouter.js
@@ -2,16 +2,43 @@ const express = require("express");
 
 const Projects = require("./data/helpers/projectModel");
 
-const router = express.Router();
+const projectRouter = express.Router();
 
-router.get("/", (req, res) => {
+projectRouter.get("/", (req, res) => {
   Projects.get()
     .then(projects => {
       res.status(200).json(projects);
     })
     .catch(error => {
-      message: error.message;
+      res.status(500).json({
+        message: error.message
+      });
     });
 });
 
-module.exports = router;
+projectRouter.post("/", (req, res) => {
+  const post = {
+    name: req.body.name,
+    description: req.body.description
+  };
+  Projects.insert(post)
+    .then(project => {
+      if (!post.name || !post.description) {
+        res.status(400).json({
+          message: "mandatory fields name and description required"
+        });
+      } else {
+        post.id = project.id;
+        res.status(200).json({
+          project
+        });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        message: error.message
+      });
+    });
+});
+
+module.exports = projectRouter;

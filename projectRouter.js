e62dade62e455e8cad774cef0d6e663120ab5406
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-01 10:05:52 +0000

    created a get endpoint to fetch projects with specified ids

--- projectRouter.js
+++ projectRouter.js
@@ -16,6 +16,26 @@ projectRouter.get("/", (req, res) => {
     });
 });
 
+projectRouter.get("/:id", (req, res) => {
+  Projects.get(req.params.id)
+    .then(project => {
+      if (!project) {
+        res.status(400).json({
+          message: "The project with the specified ID does not exist."
+        });
+      } else {
+        res.status(200).json({
+          project
+        });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "this content could not be retrieved"
+      });
+    });
+});
+
 projectRouter.post("/", (req, res) => {
   const post = {
     name: req.body.name,
@@ -41,48 +61,48 @@ projectRouter.post("/", (req, res) => {
     });
 });
 projectRouter.put("/:id", (req, res) => {
-    if (!req.body.name || !req.body.description) {
-      res.status(400).json({
-        errorMessage: "Please provide name and description to edit this project."
-      });
-    } else {
-      Projects.update(req.params.id, req.body)
-        .then(updated => {
-          if (!updated) {
-            res.status(404).json({
-              message: "The project with the specified ID does not exist."
-            });
-          } else {
-            res.status(200).json({
-              updated: req.body
-            });
-          }
-        })
-        .catch(() => {
-          res.status(500).json({
-            error: "The project information could not be modified."
-          });
-        });
-    }
-  });
-
-  projectRouter.delete("/:id", (req, res) => {
-    const { id } = req.params;
-    Projects.remove(id)
-      .then(deleted => {
-        if (!deleted) {
+  if (!req.body.name || !req.body.description) {
+    res.status(400).json({
+      errorMessage: "Please provide name and description to edit this project."
+    });
+  } else {
+    Projects.update(req.params.id, req.body)
+      .then(updated => {
+        if (!updated) {
           res.status(404).json({
             message: "The project with the specified ID does not exist."
           });
         } else {
-          res.status(201).end();
+          res.status(200).json({
+            updated: req.body
+          });
         }
       })
       .catch(() => {
         res.status(500).json({
-          error: "This project could not be removed"
+          error: "The project information could not be modified."
         });
       });
-  });
+  }
+});
+
+projectRouter.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  Projects.remove(id)
+    .then(deleted => {
+      if (!deleted) {
+        res.status(404).json({
+          message: "The project with the specified ID does not exist."
+        });
+      } else {
+        res.status(201).end();
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "This project could not be removed"
+      });
+    });
+});
 
 module.exports = projectRouter;

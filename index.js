fe71f63d29823415d33099dc90a1b9c3d3e9b148
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-01 08:16:59 +0000

    created a server file and have server listening on port 7000 in index.js

--- index.js
+++ index.js
@@ -12,3 +12,9 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+
+const server = require("./server");
+
+server.listen(7000, () => {
+  console.log("\n* Server Running on http://localhost:7000 *\n");
+});
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const actionRouter = require("");
+
+const projectRouter = require("");
+
+
+const helmet = require('helmet')
+
+server.use(helmet());
+
+
+const server = express();

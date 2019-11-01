725601c30d7d715cce8c45ae676681a80d33fa13
oladimeji-ojo <ojokuredim@gmail.com>
2019-11-01 10:34:13 +0000

    created a post endpoint to post actions

--- actionRouter.js
+++ actionRouter.js
@@ -5,36 +5,61 @@ const Actions = require("./data/helpers/actionModel");
 const actionRouter = express.Router();
 
 actionRouter.get("/", (req, res) => {
-    Actions.get()
-      .then(actions => {
-        res.status(200).json(actions);
-      })
-      .catch(() => {
-        res.status(500).json({
-          error: "this content could not be retrieved"
-        });
+  Actions.get()
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "this content could not be retrieved"
       });
-  });
+    });
+});
 
-  actionRouter.get("/:id", (req, res) => {
-    Actions.get(req.params.id)
-      .then(action => {
-        if (!action) {
-          res.status(400).json({
-            message: "The action with the specified ID does not exist."
-          });
-        } else {
-          res.status(200).json({
-            action
-          });
-        }
-      })
-      .catch(() => {
-        res.status(500).json({
-          error: "this content could not be retrieved"
+actionRouter.get("/:id", (req, res) => {
+  Actions.get(req.params.id)
+    .then(action => {
+      if (!action) {
+        res.status(400).json({
+          message: "The action with the specified ID does not exist."
         });
+      } else {
+        res.status(200).json({
+          action
+        });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "this content could not be retrieved"
       });
-  });
+    });
+});
 
+actionRouter.post("/", (req, res) => {
+  const post = {
+    description: req.body.description,
+    note: req.body.note
+  };
+  actions
+    .insert(post)
+    .then(action => {
+      if (!post.description || !post.note) {
+        res.status(400).json({
+          message: "mandatory fields note and description required"
+        });
+      } else {
+        post.id = action.id;
+        res.status(200).json({
+          action
+        });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({
+        error: "This action could not be posted"
+      });
+    });
+});
 
 module.exports = actionRouter;
